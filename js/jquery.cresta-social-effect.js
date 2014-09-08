cf197ad7ea9575a64ff1acfb3e2b55ce048b0750
CrestaProject <CrestaProject@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-08 13:20:13 +0000

    Update
    git-svn-id: https://plugins.svn.wordpress.org/cresta-social-share-counter/trunk@984311 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/jquery.cresta-social-effect.js
@@ -0,0 +1,13 @@
+(function($) {
+	"use strict";
+	
+	$(document).ready(function() {
+		
+		$("#crestashareicon .sbutton").each(function(index) {
+			$(this).delay(150*index).fadeIn(500);
+		});	
+		
+
+	});
+	
+})(jQuery);
